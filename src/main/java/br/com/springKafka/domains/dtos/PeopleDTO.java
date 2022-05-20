ae14d883678d2eec31d891bff0dde0740a8159d8
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-05-20 17:57:53 -0300

    criando classe de PeopleDTO

---
+++ src/main/java/br/com/springKafka/domains/dtos/PeopleDTO.java
@@ -0,0 +1,12 @@
+package br.com.springKafka.domains.dtos;
+
+import lombok.Getter;
+
+import java.util.List;
+@Getter
+public class PeopleDTO {
+
+    private String name;
+    private String cpf;
+    private List<String> books;
+}
